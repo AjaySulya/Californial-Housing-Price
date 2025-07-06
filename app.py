3653b8c6f0a27a477977ebdb4e68ab218b7faeee
Ajay Sulya <ajaysulya06@gmail.com>
2025-07-06 17:16:33 +0530

    This is the commit for Web application

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import pandas as pd
 
 
 app = Flask(__name__)
+##3 load the scaler 
+scaler = pickle.load(open('scaler.pkl','rb'))
+### load the model 
 regmodel = pickle.load(open('regmodel.pkl','rb'))
 
 ### this is home page 
@@ -13,7 +16,22 @@ regmodel = pickle.load(open('regmodel.pkl','rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',method=['POST'])
+
+### 
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
-    data = request.josn['data']
+    data = request.json['data']
     print(data)
+    ## prepairing the data for standardization
+    print(np.array(list(data.values())).reshape(1,-1)) ## data.values() in dict format so, 
+    ## making into list then reshape that dict values into one row and required columns 
+    
+    ### new scaled data 
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
